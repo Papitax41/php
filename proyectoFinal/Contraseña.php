495ed231e109ce5db8d650719bdcbd5abac02b82
Papita <202000672k@utea.edu.pe>
2023-09-30 20:52:30 -0500

    commit x

--- proyectoFinal/Contraseña.php
+++
@@ -1,8 +0,0 @@
-<?php
-function valida($usuario, $clave1, $clave2)
-{
-    $acceso = '';
-    if ($usuario == 'papita' && $clave1 == '2222A' && $clave2 == '2222A') $acceso = 'ok';
-    return $acceso;
-}
-?>
